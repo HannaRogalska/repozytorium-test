72f8deade9ce64951338eda8252adfbc3ae6e136
Hanna Rogalska <hanrogalska@gmail.com>
2023-02-10 14:45:55 +0100

    html

---
+++ main.js
@@ -0,0 +1,75 @@
+// let myName = "Hann";
+// console.log("Hello, " + myName);
+// if (myName === "Hanna") {
+//     console.log("Hello me");
+// } else {
+//     console.log("Hello first person")
+// }
+// let sheepCounted = 0;
+// while (sheepCounted < 10) {
+//     console.log("Number sheep " + sheepCounted + "!");
+//     sheepCounted++;
+// }
+// console.log("Хрррррррррр-псссс");
+
+// for (let sheepCounted = 0; sheepCounted < 10; sheepCounted++){
+//      console.log("Number sheep " + sheepCounted + "!");
+// }
+// console.log("Хрррррррррр-псссс");
+// let animals = ["лев", "фламинго", "белый медведь", "удав"];
+// for (let i = 0; i < animals.length; i++) {
+//     console.log("В этом зоопарке есть" + animals[i]);
+// }
+// for (let x = 2; x < 10000; x *= 2){
+//     console.log(x)
+// }
+// for (let x = 3; x < 10000; x *= 3){
+//     console.log(x)
+// }
+// let animals = ["Кот", "Рыба", "Лемур", "Комодский варан"];
+// for (let i = 0; i < 1; i++) {
+//   console.log(
+//       animals[0] +
+//       " - прекрасное животное, " +
+//       animals[1] +
+//       " - прекрасное животное, " +
+//       animals[2] +
+//       " - прекрасное животное, " +
+//       animals[3] +
+//       " - прекрасное животное."
+//   );
+// }
+// for (let i = 3; i < 10000; i *= 3){
+//   console.log(i)
+// }
+// let x = 3;
+// while (x < 10000) {
+//   console.log(x*=3)
+// }
+// let alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+
+// let rs = "";
+// while (rs.length < 6) {
+//   rs += alphabet[Math.floor(Math.random() * alphabet.length)];
+
+// }
+// console.log(rs);
+
+// let input = "javascript is awesome";
+
+// let output = "";
+// for (let i = 0; i < input.length; i++){
+//   if (input[i] === "a") {
+//     output += 4;
+//   } else if (input[i] === "e") {
+//     output += 3;
+//   } else if (input[i] === "i") {
+//     output += "1";
+//   } else if (input[i] === "o") {
+//     output += "0";
+//   } else {
+//     output += input[i];
+//   }
+// }
+// console.log(output);
